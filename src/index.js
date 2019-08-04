2e31b770d3578c94e772d5001225647d82dfc107
Jeremy Osborne <jeremywosborne@gmail.com>
2019-08-04 09:49:57 -0700

    Try stuff out

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const fs = require('fs')
+const Ajv = require('ajv')
+
+const ajv = new Ajv({
+  useDefaults: true
+})
+const schema = JSON.parse(fs.readFileSync('./schemas/effect.schema.json'))
+const armySchema = ajv.compile(schema)
+
+const army = {}
+
+console.log('armySchema(army):', armySchema(army))
+console.log('army after side effects:', army)
