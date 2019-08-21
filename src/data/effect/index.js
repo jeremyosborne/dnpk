711b00ec07e9263e4181733fa7246f8f712caec3
Jeremy Osborne <jeremywosborne@gmail.com>
2019-08-21 10:40:47 -0700

    factory another factory
    Also correct a human derp with the `hero` effect file name.

--- src/data/effect/index.js
+++ src/data/effect/index.js
@@ -1,49 +1,10 @@
-// const debug = require('debug')
-const schema = require('../schema')
-const uuid = require('uuid/v1')
-
-// const logger = debug('dnpk/data/army')
-
-/**
- * The schema these types are derived from.
- */
-const SCHEMA_ID = `https://jeremyosborne.com/dnpk/effect.schema.json`
-
-/**
- * Create an instance of a specific effect type.
- *
- * @param {String} name the unique name of the effect we wish to load.
- */
-const create = (name) => {
-  if (!schema.isLoaded()) {
-    schema.load()
-  }
-
-  let defaultData
-  try {
-    // Mapping to file name is simple right now.
-    defaultData = require(`./${name}.json`)
-    // Instance specific data.
-    defaultData.id = uuid()
-  } catch (err) {
-    console.error(`Could not load effect of type: ${name}. Error caught:`)
-    console.error(err)
-    return
-  }
-
-  const validator = schema.ajv.getSchema(SCHEMA_ID)
-  // This will also set defaults.
-  const valid = validator(defaultData)
-  if (!valid) {
-    console.error(`Effect of type ${name} is invalid. Validation errors:`)
-    console.error(validator.errors)
-    return
-  }
-
-  return defaultData
-}
+const debug = require('debug')
+const path = require('path')
+const typeFactoryFactory = require('../type-factory-factory')
 
 // Public API.
-module.exports = {
-  create,
-}
+module.exports = typeFactoryFactory({
+  DEFS_DIR: path.resolve(path.join(__dirname, 'defs')),
+  logger: debug('dnpk/data/effect'),
+  SCHEMA_ID: 'https://jeremyosborne.com/dnpk/effect.schema.json',
+})
