7468b941150fb1de7faa361e335d1ba900fc2074
Jeremy Osborne <jeremywosborne@gmail.com>
2019-08-21 10:58:55 -0700

    fix data derp
    `hero` is not a `wizard`.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
+const chalk = require('chalk')
 const {army} = require('data')
 const l10n = require('l10n')
 const {t} = require('l10n')
 const _ = require('lodash')
 
+// Temporary view functions.
+const showGroup = (group, color) => console.log(`\t${chalk[color](group.reduce((names, instance) => {
+  names.push(instance.name)
+  return names
+}, []).join('\n\t'))}`)
+
 // int main(void)
 Promise.resolve()
   .then(() => l10n.init())
   .then(() => {
-    console.log(t('Creating armies.'))
+    console.log(t('Battle prototype'))
 
-    const armies = _.map([
-      'heavy-infantry',
-      'light-infantry',
-      'wizard',
-    ], (name) => army.create(name))
+    // Explicitly load armies vs. lazy load since we want a list to sample from.
+    army.load()
 
-    console.log(t('Armies created:'))
-    console.log(JSON.stringify(armies, null, 4))
+    const armyTypes = army.types.dir()
+    console.log(t('Armies available:'))
+    _.forEach(armyTypes, (army) => {
+      console.log(`\t${chalk.yellow(army)}`)
+    })
 
     // Create 2 groups of armies.
+
+    const dadGroup = _.times(4, () => army.create(_.sample(armyTypes)))
+    console.log('Dad the Dictator group:')
+    showGroup(dadGroup, 'blue')
+
+    const archerGroup = _.times(4, () => army.create(_.sample(armyTypes)))
+    console.log('Archer the Awesome group:')
+    showGroup(archerGroup, 'green')
 
     // Engage the 2 groups in battle.
 
