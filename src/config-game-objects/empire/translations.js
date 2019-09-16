c4b4c907e20266005c49c8be73f2497867721258
Jeremy Osborne <jeremywosborne@gmail.com>
2019-09-16 15:36:26 -0700

    add empire translations

---
+++ src/config-game-objects/empire/translations.js
@@ -0,0 +1,16 @@
+//
+// Gather translations for names of armies to aid the i18n extraction tool.
+// Nothing should look for `.js` files in this directory other than the extraction tool.
+//
+// This file is not meant to be imported or used by any other file and should not be exported.
+//
+import {t} from 'l10n'
+
+t('elvallie')
+t('grey-dwarves')
+t('horse-lords')
+t('lord-bane')
+t('orcs-of-kor')
+t('selentines')
+t('sirians')
+t('storm-giants')
