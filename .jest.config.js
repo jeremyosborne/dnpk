8d0faa879640fa439c36d15b33e827de9b4716c0
Jeremy Osborne <jeremywosborne@gmail.com>
2019-12-12 21:01:10 -0800

    chore

--- .jest.config.js
+++ .jest.config.js
@@ -1,12 +1,12 @@
 //
-// see: https://jestjs.io/docs/en/configuration#bail-number-boolean
+// see: https://jestjs.io/docs/en/configuration
 //
 module.exports = {
   // Fail quick because all of our tests should pass all the time.
   bail: 1,
   collectCoverage: true,
   coverageDirectory: "coverage",
-  // Pruned automation defaults for now since we're not doing any CI.
+  // If module is under CI, use the more extensive reporting options.
   // Default: ["json", "lcov", "text", "clover"]
   coverageReporters: ["text", "text-summary"],
 }
