bf1a6dcc385296c7a7269ed9eddeb6d61f1c3e42
Jeremy Osborne <josborne@coursera.org>
2020-08-16 12:38:31 -0700

    chore: remove duplicate translations

--- src/testing-grounds/mock-battle/index.js
+++ src/testing-grounds/mock-battle/index.js
@@ -11,7 +11,7 @@ import * as l10n from 'l10n'
 
 export const main = async () => {
   // Load expected data into memory.
-  await l10n.read({ns: ['translation']})
+  await l10n.read({ns: ['translation', 'army', 'empire']})
   await dataSourceModdables.read()
 
   mockBattle()
--- src/testing-grounds/monte-carlo/index.js
+++ src/testing-grounds/monte-carlo/index.js
@@ -10,7 +10,7 @@ import mainMenu from './main-menu'
 
 export const main = async () => {
   // Load expected data into memory.
-  await l10n.read({ns: ['translation']})
+  await l10n.read({ns: ['translation', 'army', 'empire']})
   await dataSourceModdables.read()
 
   let next = mainMenu
